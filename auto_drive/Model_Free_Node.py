3814c2fb538abccefce321ff0c7c81e4219f7fa4
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-05 10:35:27 -0500

    bigger dt

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -54,7 +54,7 @@ class Controller_Node(Node):
         self.params = params # store structure
         self.PP = PP(self.params) # pass structure to car
         self.PP.get_trajectory(self.params.wx,self.params.wy)
-        self.IP_vel = IP(alpha = 0.9, kp = 5,ki = 0.00,dt = 0.01)
+        self.IP_vel = IP(alpha = 0.9, kp = 5,ki = 0.00,dt = 0.1)
         self.IP_theta = IP(alpha = 0.25, kp = 100.0,dt = 0.001)
 
         # Publisher
