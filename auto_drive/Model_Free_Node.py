c66f835f7ccb2c68411bc2911fb3f6cabc695569
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-05 14:43:51 -0500

    brake

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -69,6 +69,7 @@ class Controller_Node(Node):
         """Callback function to process Joy messages."""
         button_pressed = msg.buttons  # List of button states (0 = released, 1 = pressed)
         self.pressed = button_pressed[5]
+        self.pressed2 = button_pressed[0]
    
 
     def pose_callback(self,msg):
@@ -92,8 +93,8 @@ class Controller_Node(Node):
         vdes = 1
         if self.pressed == 1:
             v,F = self.IP_vel.control(-v,vdes)
-        else:
-            print('paused')
+        elif self.pressed2 == 1:
+            print('brake')
             v = 0.0
             F = 0
 
