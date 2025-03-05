b77c92a73296b99e7deac97a3f256b6027c1e924
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-05 14:53:40 -0500

    f

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -93,6 +93,7 @@ class Controller_Node(Node):
         #print(v)
         #vdes,thetades = self.PP.control(x,y,v,theta)
         vdes = 1
+        F = 0
         if self.pressed == 1:
             v,F = self.IP_vel.control(-v,vdes)
         if self.pressed2 == 1:
