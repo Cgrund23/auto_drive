00be44e35c0af40f23333c03e114a8413980ae86
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-17 11:01:20 -0400

    moving

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -92,7 +92,7 @@ class Controller_Node(Node):
         #print("velocity of car")
         #print(v)
         #vdes,thetades = self.PP.control(x,y,v,theta)
-        vdes = 0
+        vdes = 0.1
         F = 0
         if self.pressed == 1:
             v,F = self.IP_vel.control(-v,vdes)
@@ -116,7 +116,7 @@ class Controller_Node(Node):
         msg.data = float(F)
         self.F.publish(msg)
         
-        self.send_vel(0.0,theta)
+        self.send_vel(v,theta)
 
     def lidar_pose_callback(self, msg):
         #print("lidar call")
