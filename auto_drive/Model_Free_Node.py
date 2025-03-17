4aa5477d41ea6b1bee498d6e823079248d0e7b1a
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-17 12:13:39 -0400

    v

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -116,7 +116,7 @@ class Controller_Node(Node):
         msg.data = float(F)
         self.F.publish(msg)
         
-        self.send_vel(0.6,-theta)
+        self.send_vel(1,-theta)
 
     def lidar_pose_callback(self, msg):
         #print("lidar call")
