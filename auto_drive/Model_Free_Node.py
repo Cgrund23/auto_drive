31cb25942caf9d513ec5a0db7f736f3390176e5d
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-17 12:39:49 -0400

    ok

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -126,8 +126,8 @@ class Controller_Node(Node):
         
         # Extract relevant distances (indexes depend on LiDAR setup)
         front_idx = len(ranges) // 2  # Directly ahead
-        left_idx = int((msg.angle_max - (3.14 / 2)) / msg.angle_increment) - 100 # 90 degrees left
-        left_idx = int(len(ranges) - front_idx * 1.5)
+        #left_idx = int((msg.angle_max - (3.14 / 2)) / msg.angle_increment) - 100 # 90 degrees left
+        left_idx = int(len(ranges) - front_idx * 1.25)
         right_idx = int((msg.angle_max + (3.14 / 2)) / msg.angle_increment)  # 90 degrees right
 
         self.front_dist = ranges[front_idx] if ranges[front_idx] > 0 else float('inf')
