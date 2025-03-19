54d4d43ea9b21df32c5ca6fda10c487ba685f0e3
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-19 11:30:01 -0400

    distance

--- auto_drive/Model_Free_Node.py
+++ auto_drive/Model_Free_Node.py
@@ -107,7 +107,7 @@ class Controller_Node(Node):
         # self.F.publish(msg)
 
         thetades = 0
-        left_des = 0.2
+        left_des = 0.5
         F = 0
         if self.pressed == 1:
             theta,F = self.IP_theta.control(x=self.left_dist, x_ref=left_des)
