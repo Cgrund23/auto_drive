41220fdf7d07a242cfac4017b56e07c259d7622c
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-03-26 10:43:57 -0400

    maybe

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -255,7 +255,7 @@ class CBF:
         #     # Optimal control input
         try:
             print(H.shape,f.shape,A.shape,b.shape)  
-            x = solve_qp(P=H, q=f, G=A, h=b, solver = "clarabel") 
+            x = solve_qp(P=H, q=f, G=A[:,0:1], h=b, solver = "clarabel") 
             print('x')
             print(x)  
             self.u = x[0]
