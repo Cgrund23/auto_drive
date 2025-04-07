c2bb03d6bfa3372a12f2a35bbdf55f8f3fd419ef
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-07 17:16:00 -0400

    test

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -166,7 +166,7 @@ class CBF:
         Computes the CBF
         """
         print(x_test.shape,X_train.shape)
-        return (x_test @ X_train)
+        return (X_train@x_test.T)
         #return 1-2*(self.rbf_kernel(x_test, X_train, length_scale, sigma_f))
         #return  self.rbf_kernel(x_test, X_train, length_scale, sigma_f) @ alpha - safe_dist
       
