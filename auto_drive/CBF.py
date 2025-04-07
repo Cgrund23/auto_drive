63e52fc6597d1ce494df6710695be4563c5f30bd
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-07 14:37:05 -0400

    pissed

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -215,7 +215,7 @@ class CBF:
         #Poe_angles = cp.arctan2(self.Poe[:,1],self.Poe[:,0]).reshape((self.Poe.shape[0],1))
         #self.PoeA = cp.hstack((self.Poe,Poe_angles))
         #K_self = self.rbf_kernel(self.Poe,X_query[:,:3],self.length_scale,self.params.sigma_f)
-        K_selfish = self.rbf_kernel(X_query[:2,:],self.Poe,self.length_scale,self.params.sigma_f)
+        K_selfish = self.rbf_kernel(X_query[:3,:],self.Poe,self.length_scale,self.params.sigma_f)
         start = time.time()
         k_inv = cp.linalg.inv(K)
         print(time.time()-start)
