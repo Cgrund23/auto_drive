6faf261507af50c4fe67cd17a164103771bf7a55
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-07 17:17:54 -0400

    weight

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -22,7 +22,7 @@ class CBF:
         self.params.sigma_f = 1.0
         
         pass
-        self.length_scale = 1.25    # found from  loop demo
+        self.length_scale = .25    # found from  loop demo
 
     # Dynamics #
 
