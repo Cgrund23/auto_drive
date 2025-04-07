c370b86e0ace962a926e0f2245fa9e4db0f9b6fa
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-07 16:40:01 -0400

    idk

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -176,8 +176,8 @@ class CBF:
         diff = x_query - X_train
         grad =  - (1 / (length_scale**2)) * diff * k_star.T
         grad_h = grad.T@k_inv
-        print(cp.hstack((grad_h, cp.zeros((grad_h.shape[0], 2)))).shape)
-        return cp.hstack((grad_h, cp.zeros((grad_h.shape[0], 2))))
+        #print(cp.hstack((grad_h, cp.zeros((grad_h.shape[0], 2)))).shape)
+        return cp.vstack((grad_h, cp.zeros(2, (grad_h.shape[0]))))
 
     def lf_cbf_function(self,dcbf):
         """
