9a98902b9f714cbdca94aca7a447166dfeccd06e
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-08 10:43:24 -0400

    message

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -268,7 +268,7 @@ class CBF:
         except Exception as e:
             print('failed constraints')
             print(f"An error occurred: {e}")
-            return [0,0],dcbf
+            return [0,0]
         
 
         
