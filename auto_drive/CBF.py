956ae7c21e3caea5c2f1e24d3183ed4299248e01
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-08 11:22:27 -0400

    spelling

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -242,7 +242,7 @@ class CBF:
 
         #H = cp.array(((1,0),(0,1)))
         H = cp.eye(3)
-        H = cp.diagnal(cp.array([1.0, 10.0, 1.0]))
+        H = cp.diagonal(cp.array([1.0, 10.0, 1.0]))
         
         f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
