c04d8856573af4a7a6e0e28f7a57227596eb4d8e
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-08 10:50:35 -0400

    neg

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -247,7 +247,7 @@ class CBF:
         H = cp.array(((1,0),(0,1)))
         H = cp.eye(3)
         
-        f = (weight_input) @ (self.u_ref).reshape(2,1)
+        f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
                  
         #     # Optimal control icput
