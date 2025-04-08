d10a3559e2ede8a388d9d0a19a52956b6d48f879
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-08 18:42:19 -0400

    tran

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -282,10 +282,10 @@ class CBF:
         b = cp.empty((0,1),float)
         LfB = {}
         LgB = {}
-        X_query = self.f_full()[:2,:]
+        X_query = self.f_full()[:2,:].T
         K = self.rbf_kernel(self.Poe,self.Poe,self.length_scale,self.params.sigma_f)
 
-        K_star = self.rbf_kernel(X_query.T,self.Poe,self.length_scale,self.params.sigma_f)
+        K_star = self.rbf_kernel(X_query,self.Poe,self.length_scale,self.params.sigma_f)
 
         k_inv = cp.linalg.inv(K)
         print(X_query.shape)
