23c5c2fcb5561b224e39d3e6c9f89e80177b5409
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-09 16:47:09 -0400

    send

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -366,7 +366,7 @@ class CBF:
        
         b = cp.vstack((b,-k.reshape((k.size,1))))
         weight_input = cp.eye(2)
-        weight_input = cp.diag(cp.array([1.0, 1.0]))
+        weight_input = cp.diag(cp.array([1.0, .0010]))
         
 
         #H = cp.array(((1,0),(0,1)))
