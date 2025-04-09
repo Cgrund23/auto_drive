01b7a82fcf14a03a515636dcf8686255724bdeb1
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-09 11:08:33 -0400

    small steer

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -327,7 +327,7 @@ class CBF:
 
         #H = cp.array(((1,0),(0,1)))
         H = cp.eye(3)
-        H = cp.diag(cp.array([1.0, 100.0, 1.0]))
+        H = cp.diag(cp.array([1.0, .10, 1.0]))
         
         f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
