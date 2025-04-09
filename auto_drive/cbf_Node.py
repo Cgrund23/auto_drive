80bea3e50f084548eb93927c1768c87a3241b2a9
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-09 16:44:06 -0400

    loook further

--- auto_drive/cbf_Node.py
+++ auto_drive/cbf_Node.py
@@ -59,7 +59,7 @@ class Controller_Node(Node):
 
             # Obstacle position
             #TODO will be from lidar and continually updated figured out
-            r_max: float = 2.0
+            r_max: float = 2.5
             cbf_gamma: float = 1.0
             # Desired target point 
             #TODO this will still exist need to find a way to relate global to local
