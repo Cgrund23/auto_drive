9a0471c9dd41c5f13e7711e000c61913dc9f51b9
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-09 09:35:37 -0400

    smoosh

--- auto_drive/cbf_Node.py
+++ auto_drive/cbf_Node.py
@@ -99,7 +99,7 @@ class Controller_Node(Node):
         #print("success")
         #print(u)
         msg = Float32MultiArray()
-        msg.data = set(state.get())
+        msg.data = set(state.flatten().get())
         self.state_publisher.publish(msg)
         self.send_vel(u[0],u[1])
         # except Exception as e:
