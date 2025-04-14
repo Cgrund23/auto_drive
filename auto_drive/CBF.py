a049c3347b70911ca0fb074a3d4c07bcb25c2b76
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-14 12:18:13 -0400

    yes

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -393,12 +393,16 @@ class CBF:
         q_torch = q_torch.to(device)
         G_torch = G_torch.to(device)
         h_torch = h_torch.to(device)
-        
+        A_torch = torch.zeros(0, P_torch.shape[0], dtype=P_torch.dtype, device=P_torch.device)
+        b_torch = torch.zeros(0, dtype=P_torch.dtype, device=P_torch.device)
+
+
         print(P_torch.shape,q_torch.shape,G_torch.shape,h_torch.shape)
         # Create and run the QP solver.
         qp_solver = QPFunction(verbose=False)
         # The QP solver returns a batched solution; squeeze the batch dimension.
-        sol = qp_solver(P_torch, q_torch, G_torch, h_torch, A_ = 0.0, b_ = 0.0)
+        sol = qp_solver(P_torch, q_torch, G_torch, h_torch, A_torch, b_torch)
+        
         print(sol)
         
         
