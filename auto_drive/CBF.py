41a9db21aab887ad91c71ada76cfc70d55cef519
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-14 12:02:54 -0400

    go

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -398,7 +398,7 @@ class CBF:
             # Create and run the QP solver.
             qp_solver = QPFunction(verbose=False)
             # The QP solver returns a batched solution; squeeze the batch dimension.
-            sol = qp_solver(P_torch, q_torch, G_torch, h_torch)
+            sol = qp_solver(P_torch, q_torch, G_torch, h_torch, A_ = None, b_ = None)
             print(sol)
             
             
