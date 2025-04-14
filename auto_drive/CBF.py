468945d84097c1f53a112b4ac39cce415567889b
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-14 15:25:29 -0400

    scale

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -369,7 +369,7 @@ class CBF:
         weight_input = cp.diag(cp.array([1.0, 10]))
         
         # H = cp.eye(3)
-        H = cp.diag(cp.array([1.0, 0.000010, 1.0]))
+        H = cp.diag(cp.array([1.0, 0.10, 1.0]))
         
         f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
