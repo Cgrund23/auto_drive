867cca3bfbc25d8a94f88b9c64f86baef27320d1
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-15 12:46:23 -0400

    small length scale

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -41,7 +41,7 @@ class CBF:
         return cp.vstack((x,y,t))
     
     def vis_barrier(
-        self, K, K_inv, training_data, Y, length_scale=0.01, sigma_f=10,
+        self, K, K_inv, training_data, Y, length_scale=0.001, sigma_f=10,
         grid_limits=((-2, 2), (-2, 2)), grid_resolution=100
     ):
         (x_min, x_max), (y_min, y_max) = grid_limits
