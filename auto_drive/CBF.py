0af1615a3362a479e4ce0432e4794cea1bd0ff6e
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-15 15:29:16 -0400

    rotate the points

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -261,6 +261,12 @@ class CBF:
 
         # Stack the computed coordinates into a 2-column matrix
         self.Poe = cp.hstack((x_lidar.reshape((-1, 1)), y_lidar.reshape((-1, 1))))
+        R90 = cp.array([[0, -1],
+                [1,  0]], dtype=cp.float32)
+
+        # Rotate self.Poe by performing a matrix multiplication.
+        # Each row in self.Poe is a point [x, y]
+        self.Poe = self.Poe @ R90.T 
 
     # def setObjects(self,distance,angle):                               
     #     """
