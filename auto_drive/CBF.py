688313e30e5946255d3773b773a6045bbdcc8884
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-15 13:05:45 -0400

    small

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -27,7 +27,7 @@ class CBF:
         self.params.sigma_f = 1.0*10**1
         
         pass
-        self.length_scale = 0.025    # found from  loop demo
+        self.length_scale = 0.03    # found from  loop demo
         self.time = 0.0
     # Dynamics #
 
