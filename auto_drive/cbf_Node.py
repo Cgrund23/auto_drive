3b86718842e2b58f61b0a5656f0e39f79d3bc777
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-15 15:06:59 -0400

    5 zeros

--- auto_drive/cbf_Node.py
+++ auto_drive/cbf_Node.py
@@ -76,8 +76,8 @@ class Controller_Node(Node):
         self.u_ref = [self.params.v,0.0]
 
         # Publisher and Subscriber
-        self.my_vel_command = self.create_publisher(AckermannDriveStamped, "/drive", 10)
-        self.state_publisher = self.create_publisher(Float32MultiArray, "/state", 10) 
+        self.my_vel_command = self.create_publisher(AckermannDriveStamped, "/ackermann_cmd", 10)
+        #self.state_publisher = self.create_publisher(Float32MultiArray, "/state", 10) 
 
     def pose_callback(self,msg):
         #print('pose')
