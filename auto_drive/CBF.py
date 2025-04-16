a33e2641212fe7c50ad5f9b31c567ca8f7ed0d03
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-16 12:33:40 -0400

    try again

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -531,8 +531,8 @@ class CBF:
         
         f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
-        self.vis_barrier(K=K,K_inv=k_inv,training_data=self.Poe, Y = self.Y, length_scale=self.length_scale, sigma_f=10, 
-                            grid_limits=((0, 1), (0, 1)), grid_resolution=400)
+        self.vis_barrier(K=K,K_inv=k_inv,training_data=self.Poe, Y = self.Y, length_scale=self.length_scale, sigma_f=1, 
+                            grid_limits=((-1, 1), (-1, 1)), grid_resolution=400)
       
         #self.vis_barrier_and_dcbf_origin(training_data=self.Poe, Y=self.Y, length_scale=self.length_scale,grid_resolution=300, sigma_f=1)
         try:
