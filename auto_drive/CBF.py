b6fca3dd9c10520c4cfe8eba8e9d682bd9f24fc4
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-04-16 14:08:43 -0400

    trans

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -502,7 +502,7 @@ class CBF:
         #print(X_query.shape)
         #k_test = self.rbf_kernel(self.f_full().T,cp.hstack((self.Poe,cp.zeros((self.Poe.shape[0],2)))).T,self.length_scale,self.params.sigma_f)
         #print(k_test.shape)
-        cbf = self.cbf_function(K_star.T,k_inv)@ self.Y
+        cbf = self.cbf_function(K_star.T,k_inv)@ self.Y.T
         print(max(abs(cbf)))
         cbf = cp.clip(cbf, -1, 1)
         dcbf = self.dcbf_function(x_query=X_query,k_star=K_star.T,X_train=self.Poe,k_inv=k_inv,length_scale=self.length_scale)
