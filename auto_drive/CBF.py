b894dad6da2c12d60b4f73bf7d38c285f7c280d3
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-08-18 14:40:51 -0400

    maybe

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -603,9 +603,9 @@ class CBF:
         b = self.lg_cbf_function(dcbf) 
         print(b.shape)
         print(self.u_ref.shape)
-        b = b @ self.u_ref
+        b = b.T @ self.u_ref
         b = b.reshape((b.size,1)) 
-        A = - (self.lf_cbf_function(dcbf) + cbf**3)
+        A = - (self.lf_cbf_function(dcbf).T + cbf**3)
         A = cp.hstack((cp.zeros((A.shape[0],1)), A , cp.zeros((A.shape[0],1))))
         
         # umax constraints
