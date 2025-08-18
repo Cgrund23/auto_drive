c98f39f74e799784119410ac63441ca404204ae4
Cgrund23 <55804653+Cgrund23@users.noreply.github.com>
2025-08-18 17:05:12 -0400

    no no viz

--- auto_drive/CBF.py
+++ auto_drive/CBF.py
@@ -546,7 +546,7 @@ class CBF:
         f = (weight_input) @ (-self.u_ref).reshape(2,1)
         f = cp.vstack((f,self.params.weightslack))
         #self.vis_barrier(K=K,K_inv=k_inv,training_data=self.Poe, Y = self.Y, length_scale=self.length_scale*2, sigma_f=1, 
-                           grid_limits=((-2, 2), (-2, 2)), grid_resolution=400)
+         #                  grid_limits=((-2, 2), (-2, 2)), grid_resolution=400)
         #self.vis_barrier_and_dcbf_origin(training_data=self.Poe, Y=self.Y, length_scale=self.length_scale,grid_resolution=300, sigma_f=1)
         try:
             x = solve_qp(P=cp.asnumpy(H), q=cp.asnumpy(f), G=cp.asnumpy(A), h=cp.asnumpy(b), solver="clarabel") 
