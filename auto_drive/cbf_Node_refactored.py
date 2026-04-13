9b5b4ceeb28ff15cc59d9977ef39762f2fb234d6
Colin Grund <CGrund@Beta.team>
2026-04-13 19:09:46 -0400

    0.1

--- auto_drive/cbf_Node_refactored.py
+++ auto_drive/cbf_Node_refactored.py
@@ -283,7 +283,7 @@ class ControllerNode(Node):
         self.omega_min = -1.0
         self.r_max = 5.0
         self.r_min_obstacle = 0.5  # Only consider obstacles closer than this (meters)
-        self.length_scale = 0.5  # Increased for smoother barrier
+        self.length_scale = 0.1  # Increased for smoother barrier
         self.sigma_f = 1.0
 
         # HOCBF parameters (from paper, Section II-C) - RELAXED FOR FEASIBILITY
